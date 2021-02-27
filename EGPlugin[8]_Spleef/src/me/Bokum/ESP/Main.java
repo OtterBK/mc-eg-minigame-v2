813f6b97674f0e455049d4c536a9889a4cefd3f9
Bokum100 <wjswodnr100@gmail.com>
2021-02-27 21:15:39 +0900

    스플리프

---
+++ EGPlugin[8]_Spleef/src/me/Bokum/ESP/Main.java
@@ -0,0 +1,632 @@
+package me.Bokum.ESP;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+import net.milkbowl.vault.economy.Economy;
+import net.milkbowl.vault.economy.EconomyResponse;
+
+public class Main extends JavaPlugin implements Listener
+{
+	public static String MS = "§f[ §aEG §f] ";
+	public static Location Lobby;
+	public static Location joinpos;
+	public static Main instance;
+	public static List<Player> plist = new ArrayList<Player>();
+	public static List<String> cooldownlist = new ArrayList<String>();
+	public static Location startpos[] = new Location[10];
+	public static int tasknum[] = new int[10];
+	public static int tasktime[] = new int[10];
+	public static ItemStack helpitem;
+	public static Inventory gamehelper;
+	public static Economy econ = null;
+	public static List<Location> blockloc = new ArrayList<Location>(300);
+	public static boolean check_start = false;
+	public static boolean check_lobbystart = false;
+	public static int Forcestoptimer = 0;
+	
+	public void onEnable()
+	{
+		instance = this;
+		getServer().getPluginManager().registerEvents(this, this);
+		getLogger().info("스플리프 플러그인이 로드 되었습니다.");
+		
+		for(int i = 0; i < tasknum.length; i++){
+			tasknum[i] = -5;
+			tasktime[i] = -5;
+		}
+		
+		gamehelper = Bukkit.createInventory(null, 27, "§c§l도우미");
+		
+		ItemStack item = new ItemStack(34, 1);
+		ItemMeta meta2 = item.getItemMeta();
+		meta2.setDisplayName("§6장식");
+		item.setItemMeta(meta2);
+		for(int i = 0; i <= 9; i++){
+			gamehelper.setItem(i, item);
+		}
+		for(int i = 17; i < 27; i++){
+			gamehelper.setItem(i, item);
+		}
+		
+		item = new ItemStack(Material.BOOK, 1);
+		meta2 = item.getItemMeta();
+		meta2.setDisplayName("§e플레이 방법");
+		item.setItemMeta(meta2);
+		gamehelper.setItem(11, item);
+		
+		item = new ItemStack(Material.BOOK, 1);
+		meta2 = item.getItemMeta();
+		meta2.setDisplayName("§e게임 규칙");
+		item.setItemMeta(meta2);
+		gamehelper.setItem(13, item);
+		
+		List<String> lorelist = new ArrayList<String>();
+		
+		helpitem = new ItemStack(345, 1);
+		ItemMeta meta1 = helpitem.getItemMeta();
+		meta1.setDisplayName("§f[ §6게임 도우미 §f]");
+		lorelist.clear();
+		lorelist.add("§f- §7우클릭시 게임 도움미 엽니다.");
+		meta1.setLore(lorelist);
+		helpitem.setItemMeta(meta1);
+		
+		Loadconfig();
+		
+        if (!setupEconomy() ) {
+            getLogger().info("[버그 발생 우려 ] Vault플러그인이 인식되지 않았습니다!");
+        }
+	}
+	
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return econ != null;
+    }
+    
+	public void onDisable()
+	{
+		getLogger().info("스플리프 플러그인이 언로드 되었습니다.");
+	}
+	
+	public void Loadconfig()
+	{
+	  try
+	  {
+		joinpos = new Location(Bukkit.getWorld(getConfig().getString("Join_world")), getConfig().getInt("Join_x"), getConfig().getInt("Join_y"), getConfig().getInt("Join_z"));
+	    Lobby = new Location(Bukkit.getWorld(getConfig().getString("Lobby_world")), getConfig().getInt("Lobby_x"), getConfig().getInt("Lobby_y"), getConfig().getInt("Lobby_z"));
+	  }
+	  catch (IllegalArgumentException e)
+	  {
+	    getLogger().info("참여 지점 또는 로비가 설정되어 있지 않습니다");
+	  }
+		ConfigurationSection sec = getConfig();
+		try
+		{
+			for(int j = 1; j <= 10; j++){
+				  startpos[j-1]= new Location(Bukkit.getWorld(sec.getString("start_world"+j)), sec.getInt("start_x"+j), sec.getInt("start_y"+j), sec.getInt("start_z"+j));
+			}
+		}
+		catch (Exception e)
+		{
+			 getLogger().info("시작 지점이 완벽히 설정 되어있지 않습니다. 버그 발생의 우려가 있습니다.");
+		}
+		try
+		{
+			for(int j = 1; j <= getConfig().getInt("blockamt"); j++){
+				  blockloc.add(new Location(getServer().getWorld("world"), getConfig().getInt("block_loc_"+j+"_x"), getConfig().getInt("block_loc_"+j+"_y"), getConfig().getInt("block_loc_"+j+"_z")));
+			}
+		}
+		catch (Exception e)
+		{
+			 getLogger().info("시작 지점이 완벽히 설정 되어있지 않습니다. 버그 발생의 우려가 있습니다.");
+		}
+	}
+	
+	public boolean onCommand(CommandSender talker, Command command, String str, String args[])
+	{
+		if(talker instanceof Player)
+		{
+			Player p = (Player) talker;
+			if(str.equalsIgnoreCase("esp") && p.isOp())
+			{
+				if(args.length <= 0)
+				{
+					Helpmessages(p);
+					return true;
+				}
+				else
+				{
+					if(args[0].equalsIgnoreCase("set"))
+					{
+						Setloc(p, args);
+						return true;
+					}
+					if(args[0].equalsIgnoreCase("join"))
+					{
+						GameJoin(p);
+						return true;
+					}
+					if(args[0].equalsIgnoreCase("quit"))
+					{
+						GameQuit(p);
+						return true;
+					}
+					if(args[0].equalsIgnoreCase("stop"))
+					{
+						Forcestop();
+						return true;
+					}
+					if(args[0].equalsIgnoreCase("test"))
+					{
+						p.teleport(startpos[Integer.valueOf(args[1])]);
+						return true;
+					}
+					if(args[0].equalsIgnoreCase("block"))
+					{
+						SaveBlock(p, args[1], args[2], args[3], args[4], args[5], args[6]);
+						return true;
+					}
+					if(args[0].equalsIgnoreCase("restore"))
+					{
+						RestoreBlock();
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+	
+	public void SaveBlock(Player p, String x1, String y1, String z1, String x2, String y2, String z2){
+		blockloc.clear();
+		int amt = 0;
+		Location pos1 = new Location(getServer().getWorld("world"), Integer.valueOf(x1), Integer.valueOf(y1), Integer.valueOf(z1));
+		Location pos2 = new Location(getServer().getWorld("world"), Integer.valueOf(x2), Integer.valueOf(y2), Integer.valueOf(z2));
+		for(int x = pos1.getBlockX(); x <= pos2.getBlockX(); x++){
+			for(int y = pos1.getBlockY(); y <= pos2.getBlockY(); y++){
+				for(int z = pos1.getBlockZ(); z <= pos2.getBlockZ(); z++){
+					Location block_loc = new Location(getServer().getWorld("world"), Integer.valueOf(x), Integer.valueOf(y), Integer.valueOf(z));
+					if(block_loc.getBlock().getType() == Material.SNOW_BLOCK){
+						amt++;
+						getConfig().set("block_loc_"+amt+"_x", block_loc.getBlockX());
+						getConfig().set("block_loc_"+amt+"_y", block_loc.getBlockY());
+						getConfig().set("block_loc_"+amt+"_z", block_loc.getBlockZ());			
+						blockloc.add(block_loc);
+					}
+				}
+			}
+		}
+		getConfig().set("blockamt", amt);
+		saveConfig();
+		Loadconfig();
+		p.sendMessage(MS+"설정완료");
+	}
+	
+	public static void RestoreBlock(){
+		for(Location loc : blockloc){
+			loc.getBlock().setType(Material.SNOW_BLOCK);
+		}
+	}
+	
+	public void Helpmessages(Player p)
+	{
+		p.sendMessage(MS+"/esp join");
+		p.sendMessage(MS+"/esp quit");
+		p.sendMessage(MS+"/esp set");
+		p.sendMessage(MS+"/esp reload");
+		p.sendMessage(MS+"/esp inv");
+	}
+	
+	public static void Forcestop()
+	{
+		Bukkit.broadcastMessage(MS+"스플리프가 강제 종료 되었습니다.");
+		for(Player p : plist)
+		{
+	          try{
+	        	  me.Bokum.EGM.Main.Spawn(p);
+	          } catch(Exception e){
+	        	  p.teleport(Main.Lobby);
+	          }
+		}
+		Cleardata();
+	}
+	
+	public static void Cleardata()
+	{
+		Bukkit.getScheduler().cancelTasks(instance);
+		Forcestoptimer = 0;
+		plist.clear();
+		check_start = false;
+		cooldownlist.clear();
+		check_lobbystart = false;
+	}
+	
+	public void Setloc(Player p, String[] args)
+	{
+		if(args.length <= 1)
+		{
+			p.sendMessage(MS+"/esp set lobby");
+			p.sendMessage(MS+"/esp set join");
+			p.sendMessage(MS+"/esp set start 1~15");
+			p.sendMessage(MS+"/esp set box 1~15");
+			return;
+		}
+		if(args.length <= 2)
+		{
+			if(args[1].equalsIgnoreCase("lobby"))
+			{
+			    getConfig().set("Lobby_world", p.getLocation().getWorld().getName());
+			    getConfig().set("Lobby_x", Integer.valueOf(p.getLocation().getBlockX()));
+			    getConfig().set("Lobby_y", Integer.valueOf(p.getLocation().getBlockY() + 1));
+			    getConfig().set("Lobby_z", Integer.valueOf(p.getLocation().getBlockZ()));
+			    saveConfig();
+			    Loadconfig();
+				p.sendMessage(MS+"로비 설정 완료");
+			}
+			else if(args[1].equalsIgnoreCase("join"))
+			{
+			    getConfig().set("Join_world", p.getLocation().getWorld().getName());
+			    getConfig().set("Join_x", Integer.valueOf(p.getLocation().getBlockX()));
+			    getConfig().set("Join_y", Integer.valueOf(p.getLocation().getBlockY() + 1));
+			    getConfig().set("Join_z", Integer.valueOf(p.getLocation().getBlockZ()));
+			    saveConfig();
+			    Loadconfig();
+				p.sendMessage(MS+"대기실 설정 완료");
+			}
+			else
+			{
+				p.sendMessage(MS+"/esp set start 1~15");
+			}
+			return;
+		}
+		else if(args.length >= 3)
+		{
+			if(args[1].equalsIgnoreCase("start"))
+			{
+					int i = Integer.valueOf(args[2]);
+					if(!(1 <= i && i <= 10))
+					{
+						p.sendMessage("1~10의 숫자만 입력 가능합니다.");
+						return;
+					}
+						getConfig().set("start_world"+i, p.getWorld().getName());
+						getConfig().set("start_x"+i, p.getLocation().getBlockX());
+						getConfig().set("start_y"+i, p.getLocation().getBlockY());
+						getConfig().set("start_z"+i, p.getLocation().getBlockZ());
+					    saveConfig();
+					    Loadconfig();
+						p.sendMessage(MS+i+"번째 시작 지점 설정 완료");
+			}
+		}
+	}
+	
+	public static void Sendmessage(String str)
+	{
+		for(Player p : plist)
+		{
+			p.sendMessage(str);
+		}
+	}
+	
+	public static void GameJoin(Player p)
+	{
+		if(plist.contains(p))
+		{
+			p.sendMessage(MS+"이미 게임에 참여중이십니다.");
+			return;
+		}
+		if(plist.size() >= 7)
+		{
+			p.sendMessage(MS+"이미 최대인원(7)입니다.");
+			return;
+		}
+		if(check_start)
+		{
+			p.sendMessage(MS+"이미 게임이 진행중입니다. 남은인원 : "+plist.size());
+			return;
+		}
+		else
+		{
+			plist.add(p);
+			p.teleport(joinpos);
+			p.getInventory().clear();
+			p.getInventory().setHelmet(null);
+			p.getInventory().setChestplate(null);
+			p.getInventory().setLeggings(null);
+			p.getInventory().setBoots(null);
+			p.getInventory().setItem(8, helpitem);
+			me.Bokum.EGM.Main.gamelist.put(p.getName(), "스플리프");
+			Sendmessage(MS+p.getName()+" 님이 스플리프에 참여하셨습니다. 인원 (§e "+plist.size()+"§7 / §c3 §f)");
+			if(!check_lobbystart && plist.size() >= 3)
+			{
+				Startgame();
+			}
+			for(Player t : plist){
+				t.playSound(t.getLocation(), Sound.NOTE_PLING, 2.0f, 1.0f);
+			}
+		}
+	}
+	
+	public void GameQuit(Player p)
+	{
+		if(!plist.contains(p))
+		{
+			return;
+		}
+		plist.remove(p);
+		if(!check_start)
+		{
+			return;
+		}
+		Sendmessage(MS+p.getName()+" §f님이 탈락되셨습니다.");
+		if(plist.size() <= 1)
+		{
+			try{
+				Win(plist.get(0));
+			}catch(Exception e){
+				
+			}
+		}
+	}
+	
+	public static void Win(final Player p)
+	{
+		p.playSound(p.getLocation(), Sound.ANVIL_LAND, 2.5f, 1.5f);
+		Sendmessage(MS+"당신이 최후로 남았습니다!");
+		try{
+		Bukkit.getScheduler().scheduleSyncDelayedTask(instance, new Runnable()
+		{
+			public void run()
+			{
+				EconomyResponse r = econ.depositPlayer(p.getName(), 400);
+				if (r.transactionSuccess()) {
+					p.sendMessage(ChatColor.GOLD + "승리 보상으로 500원을 받으셨습니다.");
+				}
+				try{
+					me.Bokum.EGM.Main.Spawn(p);
+					} catch(Exception e){
+					p.teleport(Main.Lobby);
+					}
+				Cleardata();
+				Bukkit.broadcastMessage(MS+"§b§l스플리프§f가 §9"+p.getName()+"§f님의 승리로 종료 됐습니다.");
+			}
+				}, 140L);
+		}catch(Exception e){
+	    	Forcestop();
+	    }
+	}
+	
+	public static int Getrandom(int min, int max){
+		  return (int)(Math.random() * (max-min+1)+min);
+		}
+
+	  public static void GameHelper(Player p, int slot){
+		  switch(slot){
+		  case 11:
+			  p.sendMessage("§7게임 이름 §f: §c스플리프");
+			  p.sendMessage("§f게임이 시작되면 모든 플레이어에게 삽을 지급합니다.");
+			  p.sendMessage("§f삽을 이용하여 눈블럭을 캐서");
+			  p.sendMessage("§f다른 플레이어를 떨어뜨리며 마지막까지 남으면");
+			  p.sendMessage("§f승리 하게됩니다.");
+			  p.closeInventory();
+			  return;
+			  
+		  case 13:
+			  p.sendMessage("§f삽은 버려지지 않습니다.");
+			  p.sendMessage("§f나무 막대를 우클릭 시 플래쉬 점프를 합니다.");
+			  p.sendMessage("§f섬을 만들어서 한 장소에 계속 머무는 것은 안됩니다.");
+			  p.closeInventory();
+			  return;
+			  
+		  default: return;
+		  }  
+	  }
+	  
+		public static int Getcursch()
+		{
+			for(int i = 0 ; i < tasknum.length; i++)
+			{
+				if(tasknum[i] == -5)
+				{
+					return i;
+				}
+			}
+			return 0;
+		}
+		
+		
+		public static void Canceltask(int cur)
+		{
+			Bukkit.getScheduler().cancelTask(tasknum[cur]);
+			tasknum[cur] = -5;
+			tasktime[cur] = -5;
+		}
+		
+		public static void Startgame()
+		{
+			check_lobbystart = true;
+			final int cur = Getcursch();
+			tasktime[cur] = 5;
+			Bukkit.broadcastMessage(MS+"§b§l스플리프§f가 곧 시작됩니다.");
+			tasknum[cur] = Bukkit.getScheduler().scheduleSyncRepeatingTask(instance, new Runnable()
+			{
+				public void run()
+				{
+					if(tasktime[cur] > 0)
+					{
+						Sendmessage(MS+"게임이 "+tasktime[cur]*10+" 초 후 시작됩니다.");
+						tasktime[cur]--;
+						if(tasktime[cur] == 3){
+							Bukkit.broadcastMessage(MS+"§a10초 후 §b스플리프§f의 맵을 초기화합니다. §c렉에 주의해주세요!");
+						}
+						if(tasktime[cur] == 2){
+							Bukkit.broadcastMessage(MS+"§b스플리프§f의 맵을 초기화합니다. §c렉주의!!!");
+							RestoreBlock();
+						}
+					}
+					else
+					{
+						Canceltask(cur);
+						check_start = true;
+						for(int i = 0; i < plist.size(); i++){
+							Player p = plist.get(i);
+							p.getInventory().clear();
+							p.playSound(p.getLocation(), Sound.ANVIL_LAND, 2.0f, 1.0f);
+							p.sendMessage(MS+"게임이 시작 됐습니다.");
+							p.teleport(startpos[i]);
+							p.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 72000, 1));
+							p.getInventory().addItem(new ItemStack(Material.DIAMOND_SPADE, 1));
+							p.getInventory().addItem(new ItemStack(Material.STICK, 1));
+						}
+					}
+				}
+			}, 200L, 200L);
+		}
+		
+		@EventHandler
+		public void onOpenChestBlock(PlayerInteractEvent e)
+		{
+			if(e.getAction() == Action.RIGHT_CLICK_BLOCK || e.getAction() == Action.RIGHT_CLICK_AIR)
+			{
+				final Player p = e.getPlayer();
+				if(!plist.contains(p) || e.getItem() == null) return;
+				if(e.getItem().getType() == Material.COMPASS)
+						  p.openInventory(gamehelper);
+				if(e.getItem().getType() == Material.STICK && !cooldownlist.contains(p.getName())){
+					cooldownlist.add(p.getName());
+				      p.setVelocity(p.getLocation().getDirection().multiply(0.8D).setY(0.7D));
+				      p.getWorld().playSound(p.getLocation(), Sound.BAT_TAKEOFF, 2.0f, 0.8f);
+				      Bukkit.getScheduler().scheduleSyncDelayedTask(this, new Runnable(){
+				    	  public void run(){
+				    		  cooldownlist.remove(p.getName());
+				    		  p.sendMessage(MS+"플래쉬 점프의 쿨다운이 끝났습니다.");
+				    	  }
+				      }, 100L);
+				}
+			}
+		}
+		
+		@EventHandler
+		public void onClickInventory(InventoryClickEvent e)
+		{
+			if(e.getWhoClicked() instanceof Player)
+			{
+				Player p = (Player) e.getWhoClicked();
+				if(!plist.contains(p))
+				{
+					return;
+				}
+				 if(e.getInventory().getTitle().equalsIgnoreCase("§c§l도우미")){
+					 GameHelper(p, e.getSlot());
+					 e.setCancelled(true);
+				 }
+			}
+		}
+		
+		@EventHandler
+		public void onPlayerDamage(EntityDamageEvent e){
+			if(!(e.getEntity() instanceof Player)) return;
+			if(plist.contains((Player) e.getEntity()) && !check_start) e.setCancelled(true);
+		}
+		
+		@EventHandler
+		public void onItemDrop(PlayerDropItemEvent e){
+			if(!plist.contains(e.getPlayer())) return;
+			if(e.getItemDrop().getItemStack().getType() == Material.COMPASS || e.getItemDrop().getItemStack().getType() == Material.DIAMOND_SPADE) e.setCancelled(true);
+		}
+		
+		@EventHandler
+		public void onPlayercommand(PlayerCommandPreprocessEvent e)
+		{
+			Player p = e.getPlayer();
+			if(plist.contains(p) && (e.getMessage().equalsIgnoreCase("/스폰") || e.getMessage().equalsIgnoreCase("/spawn")
+					|| e.getMessage().equalsIgnoreCase("/넴주") || e.getMessage().equalsIgnoreCase("/ㅅㅍ")))
+			{
+				GameQuit(p);
+			}
+		}
+		
+		@EventHandler
+		public void onBlockbreak(BlockBreakEvent e){
+			  if(plist.contains(e.getPlayer())){
+				  if(!check_start) e.setCancelled(true);
+				  if(e.getBlock().getType() != Material.SNOW_BLOCK){
+						e.setCancelled(true);
+				  } else {
+					  e.getBlock().setType(Material.AIR);
+				  }
+			  }
+		}
+		  
+		@EventHandler
+		public void onBlockPlace(BlockPlaceEvent e){
+			  if(plist.contains(e.getPlayer()))
+			e.setCancelled(true);
+		}
+		
+		  @EventHandler
+		  public void onQuitPlayer(PlayerQuitEvent e) {
+		    if (plist.contains(e.getPlayer()))
+		    {
+		      GameQuit(e.getPlayer());
+		    }
+		  }
+		  
+			@EventHandler
+			public void onPlayerdeath(PlayerDeathEvent e)
+			{
+				Player p = e.getEntity();
+				if(plist.contains(p))
+				{
+					GameQuit(p);
+				}
+			}
+			
+			@EventHandler
+			public void onPvp(EntityDamageByEntityEvent e){
+				if(e.getEntity() instanceof Player && e.getDamager() instanceof Player){
+					Player p = (Player) e.getEntity();
+					Player d = (Player) e.getDamager();
+					if(plist.contains(p) && plist.contains(d)){
+						e.setCancelled(true);
+					}
+				}
+			}
+}
